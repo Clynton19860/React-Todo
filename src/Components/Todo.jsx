6d397790cb4bb486f7b6980e669d169e845ff6e6
Clynton <clynton19860@outlook.com>
2024-05-16 09:40:51 +0200

    Added State hook

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
+
 export default function Todo() {
+  const [todo, setTodo] = useState("");
   return (
     <div>
       <form>
-        <input type="text" />
-        <h1>hello</h1>
+        <input
+          onChange={(e) => setTodo(e.target.value)}
+          Value={todo}
+          type="text"
+        />
+        <button type="submit">Add</button>
       </form>
     </div>
   );
